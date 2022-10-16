3172a58695c32624c1d027744dd35a68f07b7c1c
Martinsn14 <martinsn14@gmail.com>
2022-10-16 01:39:53 -0300

    Test GITHUB

---
+++ index.js
@@ -0,0 +1,39 @@
+// mi comentario una sola linea
+
+/* comentario muchas lineas
+
+fin
+
+ctr + k + c (Comentar varias lineas)
+ctr + k + u (descomentar)
+
+Particularidades:
+
+Diferencia entre May y min, 
+No se definen tipo de variables.
+*/
+
+//para mostrar info por consola
+
+console.log('mi primera consola')
+
+//Es nesesario para DEV luego se quita
+
+
+//para interactuar con usuario, pide info al usuario
+//prompt ('Cual es tu nombre') siempre devuelve un string
+
+//para dar mensaje al usuario
+alert ('Hola estas en mi web')
+
+//declarar variable sin espacio ni caracter especial
+//let (variable puede ser global o local) - const (constante)
+
+let x = prompt ('Cual es tu edad')
+
+console.log(x)
+
+//para pasar de string a Entero parseInt() sino parseFloat()
+
+
+
