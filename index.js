44b5bfd69fc4e46a761cdcf9319ad6d24a2025ea
Martinsn14 <martinsn14@gmail.com>
2022-10-19 21:41:27 -0300

    Prueba

--- index.js
+++ index.js
@@ -24,16 +24,43 @@ console.log('mi primera consola')
 //prompt ('Cual es tu nombre') siempre devuelve un string
 
 //para dar mensaje al usuario
-alert ('Hola estas en mi web')
+//alert ('Hola estas en mi web')
 
 //declarar variable sin espacio ni caracter especial
 //let (variable puede ser global o local) - const (constante)
 
-let x = prompt ('Cual es tu edad')
+let x = parseInt(prompt('Cual es tu edad'))
 
 console.log(x)
 
 //para pasar de string a Entero parseInt() sino parseFloat()
+
+if(x >= 18) {
+
+    alert ('sos mayor de edad');
+}   else {
+    alert ('sos menor de edad');
+}
+
+//console.log(`Tu edad es ${x} `) Otra forma de concatenar variable con string
+
+/*Bucles 
+
+For (x cantidad de veces) / While / Do While (mientras se cumpla la condición)
+
+
+*/
+
+for(let i= 0; i <=10; i++) {
+    
+    console.log('Iteración N°:'+i)
+    while (i%2 == 0) {
+        alert (`El numero ${i} es par`)
+        break          
+    }
+}
+
+
 
 
 
